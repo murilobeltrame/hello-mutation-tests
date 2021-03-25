d95bdd86a68245b2f1751e02ef49e99bd7ff710e
Murilo Beltrame <murilo.beltrame@ambevtech.com.br>
2021-03-25 02:38:00 -0300

    adding tests

--- src/dotnet/HelloMutation.Domain.Tests/Entities/BookTests.cs
+++ src/dotnet/HelloMutation.Domain.Tests/Entities/BookTests.cs
@@ -1,10 +1,17 @@
 ﻿using HelloMutation.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using Xunit;
 
 namespace HelloMutation.Domain.Tests.Entities
 {
     public class BookTests
     {
+        private static readonly string _validTitle = "Book Title";
+        private static readonly IEnumerable<Author> _mockedAuthors = new Author[] { new Author("John", "Doe") };
+        private static readonly Publisher _mockedPublisher = new Publisher("Publishers House");
+
         [Fact]
         public void Should_Be_Instantiated()
         {
@@ -22,6 +29,136 @@ namespace HelloMutation.Domain.Tests.Entities
             Assert.Equal(title, book.Title);
             Assert.Contains(book.Authors, author => author.FirstName == authorFirstName && author.LastName == authorLastName);
             Assert.Equal(publisherName, book.Publisher.Name);
+        }
+
+        [Fact]
+        [SuppressMessage("Performance", "CA1806:Do not ignore method results", Justification = "Should test exception while trying to instantiate de object.")]
+        public void Book_without_title_should_throw_an_ArgumentNullException()
+        {
+            static void act() => new Book(null, _mockedAuthors, _mockedPublisher);
+            var exception = Assert.Throws<ArgumentNullException>(act);
+            Assert.Equal(nameof(Book.Title), exception.ParamName);
+        }
+
+        [Fact]
+        [SuppressMessage("Performance", "CA1806:Do not ignore method results", Justification = "Should test exception while trying to instantiate de object.")]
+        public void Book_without_a_least_one_Author_should_throw_an_ArgumentException()
+        {
+            static void act() => new Book(_validTitle, null, _mockedPublisher);
+            var exception = Assert.Throws<ArgumentException>(act);
+            Assert.Equal(nameof(Book.Authors), exception.ParamName);
+        }
+
+        [Fact]
+        [SuppressMessage("Performance", "CA1806:Do not ignore method results", Justification = "Should test exception while trying to instantiate de object.")]
+        public void Book_without_a_Publisher_should_throw_an_ArgumentException()
+        {
+            static void act() => new Book(_validTitle, _mockedAuthors, null);
+            var exception = Assert.Throws<ArgumentException>(act);
+            Assert.Equal(nameof(Book.Publisher), exception.ParamName);
+        }
+
+        // PRICING
+
+        [Fact]
+        public void Setting_Price_should_accumulate_Price_record()
+        {
+            var expectedPrice = 10m;
+            var expectedDate = DateTime.Now;
+            var book = new Book(_validTitle, _mockedAuthors, _mockedPublisher);
+            book.SetPrice(expectedPrice, expectedDate);
+            Assert.Contains(book.Pricing, (price) =>
+                price.Value == expectedPrice &&
+                price.StartingAt == expectedDate);
+        }
+
+        [Fact]
+        public void Setting_Price_without_Date_should_add_a_price_with_current_Date()
+        {
+            var expectedPrice = 10m;
+            var expectedDate = DateTime.Now;
+            var book = new Book(_validTitle, _mockedAuthors, _mockedPublisher);
+            book.SetPrice(expectedPrice);
+            Assert.Contains(book.Pricing, (price) =>
+                price.Value == expectedPrice &&
+                price.StartingAt.Date == expectedDate.Date);
+        }
+
+        [Fact]
+        public void Getting_Price_at_exact_Date_should_return_the_Price()
+        {
+            var expectedPrice = 10m;
+            var expectedDate = DateTime.Now;
+            var book = new Book(_validTitle, _mockedAuthors, _mockedPublisher);
+            book.SetPrice(expectedPrice - 5, expectedDate.AddDays(-20));
+            book.SetPrice(expectedPrice, expectedDate);
+            book.SetPrice(expectedPrice + 5, expectedDate.AddDays(20));
+            var _result = book.GetPriceAt(expectedDate);
+            Assert.Equal(expectedPrice, _result.Value);
+        }
+
+        [Fact]
+        public void Getting_Price_in_inverval_should_return_the_oldest_Price()
+        {
+            var expectedPrice = 10m;
+            var expectedDate = DateTime.Now;
+            var book = new Book(_validTitle, _mockedAuthors, _mockedPublisher);
+            book.SetPrice(expectedPrice + 5, expectedDate.AddDays(20));
+            book.SetPrice(expectedPrice, expectedDate);
+            book.SetPrice(expectedPrice - 5, expectedDate.AddDays(-20));
+            var _result = book.GetPriceAt(expectedDate.AddDays(10));
+            Assert.Equal(expectedPrice, _result.Value);
+        }
+
+        [Fact]
+        public void Getting_Price_for_an_unset_Price_history_should_return_value_0_for_Today()
+        {
+            var book = new Book(_validTitle, _mockedAuthors, _mockedPublisher);
+            var result = book.GetPriceAt(DateTime.Now);
+            Assert.Equal(0m, result.Value);
+        }
+
+        // RATING
+
+        [Theory]
+        //[InlineData(3, "Not so good rate.")]
+        [InlineData(5, "Excelent rate.")]
+        public void Review_should_be_instantiated(ushort rating, string note)
+        {
+            var expectedRating = rating;
+            var expectedNote = note;
+            var review = new Review(rating, note);
+            Assert.Equal(expectedRating, review.Rating);
+            Assert.Equal(expectedNote, review.Note);
+        }
+
+        [Fact]
+        public void Placing_Review_greater_than_5_should_thrown_ArgumentOutOfRangeException()
+        {
+            var book = new Book(_validTitle, _mockedAuthors, _mockedPublisher);
+            void act() => book.PlaceReview(6);
+            Assert.Throws<ArgumentOutOfRangeException>(act);
+            Assert.Equal(0, book.Reviews.Count);
+        }
+
+        [Fact]
+        public void Getting_average_rating_should_be_the_Reviews_raging_average()
+        {
+            ushort? expectedRating = 2;
+            var book = new Book(_validTitle, _mockedAuthors, _mockedPublisher);
+            book.PlaceReview(3);
+            book.PlaceReview(1);
+            var result = book.GetAverageRating();
+            Assert.Equal(expectedRating, result);
+        }
+
+        [Fact]
+        public void Getting_average_rating_for_an_unset_Review_history_should_return_Null()
+        {
+            ushort? expectedRating = null;
+            var book = new Book(_validTitle, _mockedAuthors, _mockedPublisher);
+            var result = book.GetAverageRating();
+            Assert.Equal(expectedRating, result);
         }
     }
 }
--- src/dotnet/HelloMutation.Domain.Tests/Entities/CartTests.cs
+++ src/dotnet/HelloMutation.Domain.Tests/Entities/CartTests.cs
@@ -6,6 +6,16 @@ namespace HelloMutation.Domain.Tests.Entities
 {
     public class CartTests
     {
+        private static readonly Guid _validSessionId = Guid.NewGuid();
+        private static readonly DateTime _validDate = DateTime.Now;
+        private static readonly Book _mockedBook = new Book(
+            "A book",
+            new Author[] {
+                new Author("Jhon", "Doe")
+            },
+            new Publisher("The Publisher")
+        );
+
         [Fact]
         public void Should_Be_Instantiated()
         {
@@ -17,5 +27,131 @@ namespace HelloMutation.Domain.Tests.Entities
             Assert.Equal(sessionId, cart.SessionId);
             Assert.Equal(createdAt, cart.CreatedAt);
         }
+
+        #region ADDING ITEMS
+
+        [Fact]
+        public void Adding_an_item_for_cart_should_create_a_record()
+        {
+            ushort expectedQuantity = 1;
+            var expectedBookPrice = _mockedBook.GetPriceAt(DateTime.Now).Value;
+            var cart = new Cart(_validSessionId, _validDate);
+            cart.AddItem(_mockedBook, expectedQuantity);
+            Assert.Contains(cart.Items, (item) =>
+                item.Book == _mockedBook &&
+                item.Price == expectedBookPrice &&
+                item.Quantity == expectedQuantity);
+        }
+
+        [Fact]
+        public void Adding_an_item_twice_in_a_cart_should_accumulate_the_same_record()
+        {
+            ushort expectedQuantity = 2;
+            var expectedBookPrice = _mockedBook.GetPriceAt(DateTime.Now).Value;
+            var cart = new Cart(_validSessionId, _validDate);
+            cart.AddItem(_mockedBook, 1);
+            cart.AddItem(_mockedBook, 1);
+            Assert.Contains(cart.Items, (item) =>
+                item.Book == _mockedBook &&
+                item.Price == expectedBookPrice &&
+                item.Quantity == expectedQuantity);
+        }
+
+        [Fact]
+        public void Adding_item_of_unset_book_should_thrown_ArgumentNullException()
+        {
+            var expectedExceptionFieldName = "book";
+            var cart = new Cart(_validSessionId, _validDate);
+            void act() => cart.AddItem(null, 1);
+            var exception = Assert.Throws<ArgumentNullException>(act);
+            Assert.Equal(expectedExceptionFieldName, exception.ParamName);
+        }
+
+        #endregion
+
+        #region UPDATE ITEM QUANTITY
+
+        [Fact]
+        public void Update_item_quantity_should_change_quantity()
+        {
+            ushort expectedQuantity = 3;
+            var expectedBookPrice = _mockedBook.GetPriceAt(DateTime.Now).Value;
+            var cart = new Cart(_validSessionId, _validDate);
+            cart.AddItem(_mockedBook, 1);
+            cart.UpdateItemQuantity(_mockedBook.Title, expectedQuantity);
+            Assert.Contains(cart.Items, (item) =>
+                item.Book == _mockedBook &&
+                item.Price == expectedBookPrice &&
+                item.Quantity == expectedQuantity);
+
+        }
+
+        [Fact]
+        public void Update_item_without_a_book_should_thrown_ArgumentNullException()
+        {
+            var expectedExceptionFieldName = "bookTitle";
+            var cart = new Cart(_validSessionId, _validDate);
+            void act() => cart.UpdateItemQuantity(null, 1);
+            var exception = Assert.Throws<ArgumentNullException>(act);
+            Assert.Equal(expectedExceptionFieldName, exception.ParamName);
+        }
+
+        [Fact]
+        public void Update_an_unexisting_item_should_thrown_ArgumenteException()
+        {
+            var expectedExceptionFieldName = "bookTitle";
+            var cart = new Cart(_validSessionId, _validDate);
+            void act() => cart.UpdateItemQuantity("Unexisting Book", 3);
+            var exception = Assert.Throws<ArgumentException>(act);
+            Assert.Equal(expectedExceptionFieldName, exception.ParamName);
+        }
+
+        #endregion
+
+        #region REMOVE ITEM
+
+        [Fact]
+        public void Remove_an_item_should_wipe_record_from_items()
+        {
+            var cart = new Cart(_validSessionId, _validDate);
+            cart.AddItem(_mockedBook, 10);
+            cart.RemoveItem(_mockedBook.Title);
+            Assert.DoesNotContain(cart.Items, (item) =>
+                item.Book == _mockedBook);
+        }
+
+        [Fact]
+        public void Remove_an_unset_item_should_thrown_ArgumentNullException()
+        {
+            var expectedExceptionFieldName = "bookTitle";
+            var cart = new Cart(_validSessionId, _validDate);
+            void act() => cart.RemoveItem(null);
+            var exception = Assert.Throws<ArgumentNullException>(act);
+            Assert.Equal(expectedExceptionFieldName, exception.ParamName);
+        }
+
+        [Fact]
+        public void Remove_an_unexisting_item_should_thrown_ArgumentException()
+        {
+            var expectedExceptionFieldName = "bookTitle";
+            var cart = new Cart(_validSessionId, _validDate);
+            void act() => cart.RemoveItem("Unexisting Book");
+            var exception = Assert.Throws<ArgumentException>(act);
+            Assert.Equal(expectedExceptionFieldName, exception.ParamName);
+        }
+
+        #endregion
+
+        #region CHECKOUT
+
+        [Fact]
+        public void Should_be_possible_to_Checkout()
+        {
+            var cart = new Cart(_validSessionId, _validDate);
+            cart.Checkout();
+            Assert.True(cart.CheckedOut);
+        }
+
+        #endregion
     }
 }
