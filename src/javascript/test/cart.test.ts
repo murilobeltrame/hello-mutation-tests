8533c26cf863f59b32d705ef2573852f25b05c81
Murilo Beltrame <murilobeltrame@gmail.com>
2021-06-07 21:53:43 -0300

    finish add basic tests for js

---
+++ src/javascript/test/cart.test.ts
@@ -0,0 +1,13 @@
+import { Cart } from "../src/domain/entities/cart"
+
+describe('Cart', () => {
+    it(' should be instantiated', () => {
+        const sessionId = '1'
+        const createdAt = new Date()
+
+        const cart = new Cart({ sessionId, createdAt })
+
+        expect(cart.sessionId).toEqual(sessionId)
+        expect(cart.createdAt).toEqual(createdAt)
+    })
+})
